4f44e66b209a63dfb16ee157dc49b4e0be630242
Prawin Jayakhar <prawinkrk@gmail.com>
2026-09-19 16:28:45 +0530

    Pin langchain to 0.3.25 for stable imports

--- agent.py
+++ agent.py
@@ -5,8 +5,7 @@ from langchain_aws import AmazonKnowledgeBasesRetriever
 from langchain_core.tools import tool
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
-from langchain.agents import AgentExecutor
-from langchain_core.agents import create_tool_calling_agent
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
